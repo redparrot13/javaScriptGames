6a5de6759f783fdf1707b65fa496cf0a46da3db0
Kathleen Wilson <redparrot13@gmail.com>
2024-04-02 19:36:27 -0500

    move functions

--- snake/app.js
+++ snake/app.js
@@ -20,24 +20,49 @@ document.addEventListener('DOMContentLoaded', () => {
         clearInterval(interval)
         score = 0
         direction = 1
-        scoreDisplay.innerText = score 
+        scoreDisplay.innerText = score
         intervalTime = 1000
-        currentSnake = [2,1,0]
+        currentSnake = [2, 1, 0]
         currentIndex = 0
         currentSnake.forEach(index => squares[index].classList.add('snake'))
         interval = setInterval(moveOutcomes, intervalTime)
     }
 
     //function that handles all outcomes of the snake 
+    function moveOutcomes() {
+        //snake hitting border and hitting self
+        if (
+            (currentSnake[0] + width >= (width * width) && direction === width) || //snakes hits bottom border
+            (currentSnake[0] % width === width - 1 && direction === 1) || //snake hits right border
+            (currentSnake[0] % width === 0 && direction === -1) || //snake hits left border
+            (currentSnake[0] - width < 0 && direction === -width) ||//snake hits top border
+            squares[currentSnake[0] + direction].classList.contains('snake')
+        ) {
+            return clearInterval(interval)
+        }
+        const tail = currentSnake.pop()
+        squares[tail].classList.remove('snake') //removes class of snake from the tail
+        currentSnake.unshift(currentSnake[0] + direction) //gives direction to head of the array 
 
-    //snake hitting border and hitting self
 
-    //snake gets apple
+        //snake gets apple
+        if (squares[currentSnake[0]].classList.contains('apple')) {
+            squares[currentSnake[0]].classList.remove('apple')
+            squares[tail].classList.add('snake')
+            currentSnake.push(tail)
+            //randomApple()
+            score++
+            scoreDisplay.textConent = score
+            clearInterval(interval)
+            intervalTime = intervalTime * speed
+            interval = setInterval(moveOutcomes, intervalTime)
+        }
+        squares[currentSnake[0]].classList.add('snake')
+
+    }
 
 
 
-
-    
     //assign functions to keycodes
     function control(e) {
         console.log("key pressed: ", e.keyCode)
@@ -54,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-document.addEventListener('keyup', control)
+    document.addEventListener('keyup', control)
 
 
 
