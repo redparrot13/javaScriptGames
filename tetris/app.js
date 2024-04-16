2a53365518fb633eed624b9740054ca9a4b58fa4
Kathleen Wilson <redparrot13@gmail.com>
2024-04-16 15:49:09 -0500

    draw / undraw

--- tetris/app.js
+++ tetris/app.js
@@ -1,6 +1,69 @@
 document.addEventListener('DOMContentLoaded', () => {
-const width = 10
-const height = 20
+
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(grid.querySelectorAll('div'))
+    const width = 10
+    const height = 20
+    let currentPosition = 4
+
+
+    // tetrominoes 
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const zTetromino = [
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
+    ]
+
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ]
+
+    const iTetromino = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ]
+
+    const theTetrominos = [lTetromino, zTetromino, tTetromino
+        , oTetromino, iTetromino]
+
+    // select tetromino randomly
+    let random = Math.floor(Math.random() * theTetrominos.length)
+    let currentRotation = 0
+    let current = theTetrominos[random][currentRotation]
+
+    //draw the shape
+    function draw() {
+        current.forEach(index => (
+            squares[currentPosition + index].classList.add('block')
+        ))
+    }
+
+    //undraw the shape
+    function undraw() {
+        current.forEach(index => (
+            squares[currentPosition + index].classList.remove('block')
+        ))
+    }
 
 
 
